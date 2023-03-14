57122be650b217eb33df83bd594345a127b99f5b
Bartosz Kryscio <cichy1927@gmail.com>
2023-03-14 15:04:45 +0100

    added BEM and objects

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-const firstName = 'Bartek' ;
+const firstName = "Bartek";
 const age = 36;
-console.log(`Siema! Nazywam się ${firstName} i mam ${age} lat.`);
+
+
+function calculate(myNumber) {
+  console.log(`Mój numer to: ${myNumber}`);
+  return myNumber * 7;
+}
+
+const myResult = calculate(age);
+console.log(`Wynik funkcji to ${myResult}`);
+
+function greet(age, firstName) {
+  console.log(`Siema! Nazywam się ${firstName} i mam ${age} lat.`);
+}
+greet(age, firstName);
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+createContent('.about__description--js', 'Witaj świecie!')
